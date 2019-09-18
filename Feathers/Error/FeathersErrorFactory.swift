0cd5ce16e322d4e3eb33cea2ceca039b92f3eb61
Ostap Holub <holub.ostap@gmail.com>
2019-09-18 23:27:58 +0300

    add errors factory

---
+++ Feathers/Error/FeathersErrorFactory.swift
@@ -0,0 +1,19 @@
+//
+//  FeathersErrorFactory.swift
+//  Feathers-iOS
+//
+//  Created by Ostap Holub on 9/18/19.
+//  Copyright © 2019 Swoopy Studios. All rights reserved.
+//
+
+import Foundation
+
+class FeathersErrorFactory {
+    
+    class func makeError(failureReason reason: String) -> FeathersError {
+        let payload: [String: Any] = ["message": reason,
+                                      "name": "validation"]
+        return FeathersError(payload: payload)
+    }
+    
+}
